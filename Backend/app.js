60c761f96c2d37bf1cae74216b3625cb6b7a2deb
abhinav jha <jhaabhinav16@gmail.com>
2023-04-15 13:22:57 +0530

    initial

--- Backend/app.js
+++ Backend/app.js
@@ -32,10 +32,10 @@ app.use('/api/v1',order);
 app.use('/api/v1',contact);
 app.use('/api/v1',admin);
 
-app.use(express.static(path.join(__dirname,'../frontend/build')));
+app.use(express.static(path.join(__dirname,'/frontend/build')));
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
+    res.sendFile(path.resolve(__dirname,'/frontend/build/index.html'))
 })
 
 //middlewares for errors
