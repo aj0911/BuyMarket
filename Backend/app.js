5e37dac29bd2bf1696c3fcdbba553691f331bc98
abhinav jha <jhaabhinav16@gmail.com>
2023-04-15 20:30:41 +0530

    first Commit

--- Backend/app.js
+++ Backend/app.js
@@ -32,11 +32,11 @@ app.use('/api/v1',order);
 app.use('/api/v1',contact);
 app.use('/api/v1',admin);
 
-app.use(express.static(path.join(__dirname,'../frontend/build')));
+// app.use(express.static(path.join(__dirname,'../frontend/build')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
-})
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
+// })
 
 //middlewares for errors
 app.use(errorMiddleware);
